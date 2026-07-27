b6928f5def1a03fae4327b2c81b40b67aee6eb07
MeyanshaGurung <koolnerdthatlovesmaths@gmail.com>
2026-07-28 00:33:14 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const hearts = document.querySelector('.hearts');
+
+function createHeart() {
+    const heart = document.createElement('span');
+
+    heart.innerHTML = '💕';
+
+    heart.style.left = (Math.random() * 100) + "vw";
+
+    heart.style.animationDuration = (Math.random() * 4 + 6) + "s";
+
+    heart.style.fontSize = (Math.random() * 15 + 15) + "px";
+    
+    
+    hearts.appendChild(heart);
+    
+    
+    setTimeout(() => {
+        heart.remove();
+    }, 10000);
+}
+
+setInterval(createHeart, 400);
+
+function openLetter() {
+    window.location.href = "letter.html";
+}
+
+const music = document.getElementById("Love");
+
+function toggleMusic() {
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
