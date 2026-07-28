fad6383de79e6252c875910e8745ea9dded1fbf1
MeyanshaGurung <koolnerdthatlovesmaths@gmail.com>
2026-07-28 08:51:46 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,11 @@ const hearts = document.querySelector('.hearts');
 
 function createHeart() {
     const heart = document.createElement('span');
-
     heart.innerHTML = '💕';
-
     heart.style.left = (Math.random() * 100) + "vw";
-
     heart.style.animationDuration = (Math.random() * 4 + 6) + "s";
-
     heart.style.fontSize = (Math.random() * 15 + 15) + "px";
-    
-    
     hearts.appendChild(heart);
-    
-    
     setTimeout(() => {
         heart.remove();
     }, 10000);
@@ -22,5 +14,33 @@ function createHeart() {
 
 setInterval(createHeart, 400);
 
+const message = 'Dear shiksha';
+let i = 0;
+const speed = 45;
 
+function typeWriter() {
+    if (i < message.length) {
+        if (message.charAt(i) === '\n') {
+            document.getElementById("typewriter").innerHTML += '<br>';
+        } else {
+            document.getElementById("typewriter").innerHTML += message.charAt(i);
+        }
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function startLove() {
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('transition').style.display = 'block';
+
+    const music = document.getElementById("loveSong");
+    music.play().catch(() => {});
+
+    setTimeout(() =>{
+        document.getElementById('transition').style.display = 'none';
+        document.getElementById('letter').style.display = 'block';
+        typeWriter();
+    }, 2500);
+}
 
